5161246d732f5f6735505576b171a2787251b518
Dazdwc <dzafrad@uoc.edu>
2025-06-21 21:52:02 +0200

    feature(formatReferencePath) Se desarrolla la lógica para formatear clases según el PEP8

--- src/main/java/io/github/dazdwc/copyreference4pytest/utils/PytestPathFormatter.java
+++ src/main/java/io/github/dazdwc/copyreference4pytest/utils/PytestPathFormatter.java
@@ -1,11 +1,29 @@
 package io.github.dazdwc.copyreference4pytest.utils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class PytestPathFormatter {
     public enum PathType {
         FILE,
         CLASS_OR_FUNCTION,
         DEFAULT,
         FILE_DIRTY,
+    }
+
+    public static int obtainLastUppercaseIndex(String path) {
+        List<String> parts = Arrays.asList(path.split("\\."));
+        int lastIndex = -1;
+
+        for (int i = 0; i < parts.size(); i++) {
+            String part = parts.get(i);
+            if (!part.isEmpty() && Character.isUpperCase(part.charAt(0))) {
+                lastIndex = i;
+            }
+        }
+
+        return lastIndex;
     }
 
     public static String convert(String path, PathType type) {
@@ -27,9 +45,22 @@ public class PytestPathFormatter {
     }
 
     private static String formatReferencePath(String path) {
-        // lógica para clases o funciones (convertir a formato pytest)
-        // Ej: mi.paquete.TestClase.test_funcion -> mi/paquete.py::TestClase::test_funcion
-        return "ReferencePath(Función o clase)"; // TODO
+        List<String> parts = Arrays.asList(path.split("\\."));
+        int indexOfClass = obtainLastUppercaseIndex(path);
+        StringBuilder finalPath = new StringBuilder();
+        for (int i = 0; i < parts.size(); i++) {
+            String part = parts.get(i);
+            if (i==(indexOfClass -1) && !part.endsWith(".py")) {
+                part = part + ".py" ;
+            }if (i >= indexOfClass) {
+                part = "::" + part;
+            }else if (i > 0) {
+                part = "/" + part;
+            }
+            finalPath.append(part);
+        }
+
+        return String.valueOf(finalPath);
     }
 
     public static String formatFileDirtyPath(String path) {
